d9be611a06e20d7e668a0478d49c0029f48ae913
lars-chen <larschen05@gmail.com>
2023-12-13 18:48:51 +0100

    vae

--- vae.py
+++ vae.py
@@ -1,30 +1,38 @@
-# based on https://github.com/AntixK/PyTorch-VAE/blob/master/models/vanilla_vae.py
+import yaml
 import torch
 from torch import nn
 from torchvision import transforms
+from torch.nn import functional as F
 
-CELEB_PATH = "data"
-IMAGE_SIZE = 64
-LATENT_DIM = 128
-image_dim = 3 * IMAGE_SIZE * IMAGE_SIZE  # 67500
+# load hyperparameters from config.yml
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+celeb_path = config["data_dir"]
+image_size = config["image_size"]
+latent_dim = config["latent_dim"]
 
 
 celeb_transform = transforms.Compose(
     [
-        transforms.Resize(IMAGE_SIZE, antialias=True),
-        transforms.CenterCrop(IMAGE_SIZE),
+        transforms.Resize(image_size, antialias=True),
+        transforms.CenterCrop(image_size),
         transforms.ToTensor(),
     ]
 )  # used when transforming image to tensor
 
 celeb_transform1 = transforms.Compose(
-    [transforms.Resize(IMAGE_SIZE, antialias=True), transforms.CenterCrop(IMAGE_SIZE)]
+    [transforms.Resize(image_size, antialias=True), transforms.CenterCrop(image_size)]
 )  # used by decode method to transform final output
 
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, image_size, beta):
         super(VAE, self).__init__()
+
+        self.image_size = image_size
+        self.image_dim = 3 * image_size * image_size
+        self.beta = beta
 
         hidden_dims = [32, 64, 128, 256, 512]
         self.final_dim = hidden_dims[-1]
@@ -49,15 +57,15 @@ class VAE(nn.Module):
             in_channels = h_dim
 
         self.encoder = nn.Sequential(*modules)
-        out = self.encoder(torch.rand(1, 3, IMAGE_SIZE, IMAGE_SIZE))
+        out = self.encoder(torch.rand(1, 3, image_size, image_size))
         self.size = out.shape[2]
-        self.fc_mu = nn.Linear(hidden_dims[-1] * self.size * self.size, LATENT_DIM)
-        self.fc_var = nn.Linear(hidden_dims[-1] * self.size * self.size, LATENT_DIM)
+        self.fc_mu = nn.Linear(hidden_dims[-1] * self.size * self.size, latent_dim)
+        self.fc_var = nn.Linear(hidden_dims[-1] * self.size * self.size, latent_dim)
 
         # Build Decoder
         modules = []
         self.decoder_input = nn.Linear(
-            LATENT_DIM, hidden_dims[-1] * self.size * self.size
+            latent_dim, hidden_dims[-1] * self.size * self.size
         )
         hidden_dims.reverse()
 
@@ -120,3 +128,11 @@ class VAE(nn.Module):
         mu, log_var = self.encode(x)
         z = self.reparameterize(mu, log_var)
         return self.decode(z), mu, log_var
+
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    def loss_function(self, recon_x, x, mu, log_var):
+        MSE = F.mse_loss(recon_x, x.view(-1, self.image_dim))
+        KLD = -0.5 * torch.mean(1 + log_var - mu.pow(2) - log_var.exp())
+        kld_weight = 0.00025
+        loss = MSE + kld_weight * KLD
+        return loss
